ab9280672afb2173bfbf22cd58c7eb6bff41e39d
niplav <niplav@posteo.net>
2022-07-16 15:09:54 +0200

    Fixed questions data as to be usable.

--- metaculus.py
+++ metaculus.py
@@ -7,11 +7,12 @@ import pandas as pd
 import iqisa
 
 class PrivateBinary(iqisa.ForecastSetHandler):
-	def load(self, files):
-		self.load_complete(files)
+	question_data_file='./data/metaculus/questions.json'
+	def load(self, data_file):
+		self.load_complete(data_file)
 
-	def load_complete(self, files):
-		f=open(files)
+	def load_complete(self, data_file):
+		f=open(data_file)
 		jsondata=json.load(f)
 
 		user_ids=[]
@@ -22,12 +23,14 @@ class PrivateBinary(iqisa.ForecastSetHandler):
 		outcomes=[]
 		open_times=[]
 		resolve_times=[]
+		question_titles=[]
 
 		for question in jsondata:
 			if question['question_type']=='binary':
 				resolution=str(question['resolution'])
 				open_time=pd.to_datetime(question['publish_time'])
 				resolve_time=pd.to_datetime(question['resolve_time'])
+				question_title=str(question['question_title'])
 				numf=0
 				for forecast in question['prediction_timeseries']:
 					user_ids.append(int(forecast['user_id']))
@@ -37,6 +40,7 @@ class PrivateBinary(iqisa.ForecastSetHandler):
 				open_times+=[open_time]*numf
 				resolve_times+=[resolve_time]*numf
 				outcomes+=[resolution]*numf
+				question_titles+=[resolution]*numf
 		numf=len(probabilities)
 		answer_options=['1']*numf
 		outcomes=['1']*numf
@@ -48,6 +52,25 @@ class PrivateBinary(iqisa.ForecastSetHandler):
 
 		self.forecasts=pd.DataFrame({'user_id': user_ids, 'team_id': team_ids, 'probability': probabilities, 'answer_option': answer_options, 'timestamp': timestamps, 'outcome': outcomes, 'open_time': open_times, 'resolve_time': resolve_times, 'n_opts': n_opts, 'options': options, 'q_status': q_status, 'q_type': q_type})
 
-	def load_questions(self, files):
-		return
-		# TODO: do stuff
+	def load_questions(self, data_file=None):
+		if data_file==None:
+			data_file=self.question_data_file
+		f=open(data_file)
+		jsondata=json.load(f)
+
+		question_ids=[]
+		open_times=[]
+		close_times=[]
+		resolve_times=[]
+		outcomes=[]
+		question_titles=[]
+
+		for question in jsondata:
+			question_ids.append(int(question['question_id']))
+			open_times.append(pd.to_datetime(question['open_time']))
+			close_times.append(pd.to_datetime(question['close_time']))
+			resolve_times.append(pd.to_datetime(question['resolve_time']))
+			outcomes.append(question['outcome'])
+			question_titles.append(question['question_title'])
+
+		self.questions=pd.DataFrame({'question_id': question_ids, 'q_text': question_titles, 'open_time': open_times, 'close_time': close_times, 'resolve_time': resolve_times, 'outcome': outcomes})
