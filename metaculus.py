2d0c73778731f54ee6f7dfcf4888fd5fb9cb5ea0
niplav <niplav@posteo.net>
2022-07-16 12:49:59 +0200

    Work on the Metaculus private data loader.

--- metaculus.py
+++ metaculus.py
@@ -6,34 +6,11 @@ import pandas as pd
 
 import iqisa
 
-comparable_index=['question_id', 'user_id', 'team_id', 'probability', 'answer_option', 'timestamp', 'outcome', 'open_time', 'date_suspend', 'date_to_close', 'date_closed', 'days_open', 'n_opts', 'options', 'q_status', 'q_type']
-
 class PrivateBinary(iqisa.ForecastSetHandler):
 	def load(self, files):
 		self.load_complete(files)
 
-	def load_complete_1(self, files):
-		self.forecasts=pd.DataFrame(columns=comparable_index)
-		f=open(files)
-		jsondata=json.load(f)
-		for question in jsondata:
-			if question['question_type']=='binary':
-				resolution=str(question['resolution'])
-				open_time=pd.to_datetime(question['publish_time'])
-				resolve_time=pd.to_datetime(question['resolve_time'])
-				user_ids=[]
-				probabilities=[]
-				timestamps=[]
-				for forecast in question['prediction_timeseries']:
-					user_ids.append(int(forecast['user_id']))
-					probabilities.append(float(forecast['prediction']))
-					timestamps.append(pd.to_datetime(forecast['timestamp']))
-				numf=len(probabilities)
-				newrows=pd.DataFrame({'user_id': user_ids, 'team_id': np.repeat([0], numf), 'probability': probabilities, 'answer_option': np.repeat(['1'], numf), 'timestamp': timestamps, 'outcome': np.repeat([resolution], numf), 'open_time': np.repeat([open_time], numf), 'resolve_time': np.repeat([resolve_time], numf)})
-				self.forecasts=self.forecasts.append(newrows)
-
 	def load_complete(self, files):
-		self.forecasts=pd.DataFrame(columns=comparable_index)
 		f=open(files)
 		jsondata=json.load(f)
 
@@ -64,8 +41,12 @@ class PrivateBinary(iqisa.ForecastSetHandler):
 		answer_options=['1']*numf
 		outcomes=['1']*numf
 		team_ids=[0]*numf
+		n_opts=[2]*numf
+		options=['(0) No, (1) Yes']*numf
+		q_status=['resolved']*numf
+		q_type=[0]*numf
 
-		self.forecasts=pd.DataFrame({'user_id': user_ids, 'team_id': team_ids, 'probability': probabilities, 'answer_option': answer_options, 'timestamp': timestamps, 'outcome': outcomes, 'open_time': open_times, 'resolve_time': resolve_times})
+		self.forecasts=pd.DataFrame({'user_id': user_ids, 'team_id': team_ids, 'probability': probabilities, 'answer_option': answer_options, 'timestamp': timestamps, 'outcome': outcomes, 'open_time': open_times, 'resolve_time': resolve_times, 'n_opts': n_opts, 'options': options, 'q_status': q_status, 'q_type': q_type})
 
 	def load_questions(self, files):
 		return
