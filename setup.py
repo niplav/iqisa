94f5507caa90b152ad41f24d219c9abd3ae8e141
Peter Hrosso <hrosspet@gmail.com>
2023-04-21 11:03:13 +0200

    CHG: move things around to create a python module which can be imported from any location

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup  # type: ignore
+
+if __name__ == "__main__":
+    setup(
+        name="iqisa",
+        version=0,
+        packages=["iqisa"],
+        description="A Library for Handling Forecasting Datasets",
+        long_description="",
+        url="https://github.com/niplav/iqisa/",
+    )
